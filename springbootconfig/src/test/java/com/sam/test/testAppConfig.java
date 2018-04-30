bb4215bd26b71e38603e6a32c4fcecfdcf4a5258
samlv <501634089@qq.com>
2018-04-30 15:24:52 +0800

    up

--- springbootconfig/src/test/java/com/sam/test/testAppConfig.java
+++ springbootconfig/src/test/java/com/sam/test/testAppConfig.java
@@ -5,5 +5,8 @@ public class testAppConfig {
 	public void main(String args[])
 	{
 		System.out.println("ni hao a ");
+		System.out.println("我是dev1.4");
+		System.out.println("ni hao a ");
+		System.out.println("我是dev1.4");
 	}
 }
